2f8e6612282aa345edfd9a8d9462a27364ba3930
Suzan Sucro <suzansucro@gmail.com>
2017-08-01 20:18:12 -0700

    Mess with easing

--- 06-Adding-Tweens-To-Timeline/end/src/js/main.js
+++ 06-Adding-Tweens-To-Timeline/end/src/js/main.js
@@ -1,5 +1,5 @@
 (function($) {
-    
+
 	var img = $('img'),
 		h2 = $('h2'),
 		h1 = $('h1'),
@@ -12,8 +12,9 @@
 		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
 		.add('intro')
 		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')
-		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro');
+		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=1')
+		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=2')
+		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')
+    .from(listItem, 1, {opacity:1, delay:1, ease:Strong.easeIn})
 
 })(jQuery);
